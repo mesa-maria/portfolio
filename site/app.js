16588e4d73dea47cd764035c01522c6f151d1ee3
Maria Mesa <mariagmesa37@gmail.com>
2026-03-10 13:54:50 +0100

    feat: add render for finished art

--- site/app.js
+++ site/app.js
@@ -1,18 +1,30 @@
-const ART = {
-  finished: [
-    "./assets/art/finished/obra-1.jpg",
-    "./assets/art/finished/obra-2.jpg",
-  ],
-  sketchbook: [
-    "./assets/art/sketchbook/page-1.jpg",
-    "./assets/art/sketchbook/page-2.jpg",
-  ],
-  animations: ["./assets/art/animations/reel-1.mp4"],
-  crafts: {
-    "project-1": [
-      "./assets/art/crafts/project-1-1.jpg",
-      "./assets/art/crafts/project-1-2.jpg",
-    ],
-    "project-2": ["./assets/art/crafts/project-2-1.jpg"],
-  },
-};
+import { ART } from "./art-dictionary.js";
+
+document.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  renderFinished();
+  renderSketchbook();
+  renderAnimations();
+  renderCrafts();
+  renderContact();
+}
+
+function renderFinished() {
+  const container = document.querySelector(".finished-content");
+
+  ART.finished.forEach((src) => {
+    const img = createImage(src);
+    container.appendChild(img);
+  });
+}
+
+function createImage(src) {
+  const img = document.createElement("img");
+
+  img.src = src;
+  img.className = "art-image";
+  img.loading = "lazy";
+
+  return img;
+}
---
+++ site/art-dictionary.js
@@ -0,0 +1,44 @@
+export const ART = {
+  finished: [
+    "./assets/art/finished/a.jpg",
+    "./assets/art/finished/b.jpg",
+    "./assets/art/finished/c.jpg",
+    "./assets/art/finished/d.jpg",
+    "./assets/art/finished/e.jpg",
+    "./assets/art/finished/f.jpg",
+    "./assets/art/finished/g.jpg",
+    "./assets/art/finished/h.jpg",
+    "./assets/art/finished/i.jpg",
+    "./assets/art/finished/j.jpg",
+    "./assets/art/finished/k.jpg",
+    "./assets/art/finished/l.jpg",
+    "./assets/art/finished/m.jpg",
+    "./assets/art/finished/n.jpg",
+    "./assets/art/finished/o.jpg",
+  ],
+  sketchbook: [
+    "./assets/art/sketchbook/page-1.jpg",
+    "./assets/art/sketchbook/page-2.jpg",
+    "./assets/art/sketchbook/page-3.jpg",
+    "./assets/art/sketchbook/page-4.jpg",
+    "./assets/art/sketchbook/page-5.jpg",
+    "./assets/art/sketchbook/page-6.jpg",
+    "./assets/art/sketchbook/page-7.jpg",
+    "./assets/art/sketchbook/page-8.jpg",
+    "./assets/art/sketchbook/page-9.jpg",
+  ],
+  animations: [
+    "./assets/art/animations/animation-1.mp4, ./assets/art/animations/animation-2.mp4",
+  ],
+  crafts: {
+    "project-1": [
+      "./assets/art/crafts/craft-a-1.jpg",
+      "./assets/art/crafts/craft-a-2.jpg",
+      "./assets/art/crafts/craft-a-3.jpg",
+    ],
+    "project-2": [
+      "./assets/art/crafts/craft-b-1.jpg",
+      "./assets/art/crafts/craft-b-2.jpg",
+    ],
+  },
+};
