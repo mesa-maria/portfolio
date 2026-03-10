c748d74f0f1df8ef89847194ca5e8c5d261f934c
Maria Mesa <mariagmesa37@gmail.com>
2026-03-10 13:16:30 +0100

    fix: using relative routes

---
+++ site/app.js
@@ -0,0 +1,18 @@
+const ART = {
+  finished: [
+    "./assets/art/finished/obra-1.jpg",
+    "./assets/art/finished/obra-2.jpg",
+  ],
+  sketchbook: [
+    "./assets/art/sketchbook/page-1.jpg",
+    "./assets/art/sketchbook/page-2.jpg",
+  ],
+  animations: ["./assets/art/animations/reel-1.mp4"],
+  crafts: {
+    "project-1": [
+      "./assets/art/crafts/project-1-1.jpg",
+      "./assets/art/crafts/project-1-2.jpg",
+    ],
+    "project-2": ["./assets/art/crafts/project-2-1.jpg"],
+  },
+};
